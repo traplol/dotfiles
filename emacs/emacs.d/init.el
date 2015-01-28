287dc6dad09269b1a555c52f0fc0a924e8f7836e
Max Mickey <max.mickey1991@gmail.com>
2015-01-27 18:28:11 -0800

    see whole commit message
    - left in a short circuit in the `my:packman-install-package' function that
    stopped the check to see if a packager is already installed.
    - now correctly generates the `packages.el' file if it doesn't exist
    - now correctly binds `my:packman-package-list' if it has not been bound
    yet
    - included a warning message in `packages.el' to convey that it is an
    automatically generated file.

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -21,7 +21,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
- ;; Grey background
- (set-face-background 'default "#0A0A0A")
- )
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "green"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "cyan"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "salmon"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "dark green"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "deep pink"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "turquoise4"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "medium blue"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "chocolate")))))
