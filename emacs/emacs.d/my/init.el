affbec5818da30217daceb194cc402d7685c4d88
Max Mickey <max.mickey1991@gmail.com>
2015-02-24 12:39:05 -0600

    emacs: added some packages
    irony-mode
    flycheck-irony
    company-irony

--- emacs/emacs.d/my/init.el
+++ emacs/emacs.d/my/init.el
@@ -24,6 +24,7 @@
 ;; evil-mode
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/evil"))
 (require 'evil)
+(evil-mode 1)
 ;; Use Emacs keybindings when in insert mode.
 (setcdr evil-insert-state-map nil)
 (define-key evil-insert-state-map [escape] 'evil-normal-state)
@@ -44,3 +45,22 @@
   (let ((value (eval (preceding-sexp))))
     (kill-sexp -1)
     (insert (format "%S" value))))
+
+;; irony-mode
+(defun my:c-mode-hook ()
+  (company-mode 1)
+  (company-irony 1)
+  (flycheck-mode 1)
+  (irony-mode 1))
+
+(add-hook 'c++-mode-hook 'my:c-mode-hook)
+(add-hook 'c-mode-hook 'my:c-mode-hook)
+(add-hook 'objc-mode-hook 'my:c-mode-hook)
+
+(defun my:irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+	'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+	'irony-completion-at-point-async))
+(add-hook 'irony-mode-hook 'my:irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
